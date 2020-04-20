833b11fa3fe3f7945c410ecaaaa12a0cb8c8b39a
Mikhail Semin <mikhail.semin@emlid.com>
2020-04-20 07:27:58 +0300

    remove extra await

--- pumpUpProxies.js
+++ pumpUpProxies.js
@@ -9,7 +9,7 @@ export const pumpUpProxies = async (lastPage = 10) => {
   console.log("Pumpying the proxies' list...");
 
   for (let page = 1; page < lastPage; page++) {
-    list.push(...(await await proxyList.fetchProxiesList(page)));
+    list.push(...(await proxyList.fetchProxiesList(page)));
   }
 
   console.log(`Pumped up: ${list.length} proxies`);
