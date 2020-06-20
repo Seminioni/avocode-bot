8e78d97d866478069079090e9fb8ebda0e78fd2b
Mikhail Semin <mikhail.semin@emlid.com>
2020-06-20 13:39:34 +0500

    remove console.logs and redundant headers

--- registrerAvocodeTrialAccount.js
+++ registrerAvocodeTrialAccount.js
@@ -8,65 +8,30 @@ const register = async (page, credentials) => {
   const response = await page.evaluate(async (credentials) => {
     credentials = JSON.parse(credentials);
 
-    console.log("Fetching csrf-token");
-    const csrftoken = await fetch(
-      "https://manager.avocode.com/api/get-csrftoken/",
-      {
-        credentials: "include",
-        headers: {
-          "User-Agent":
-            "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:75.0) Gecko/20100101 Firefox/75.0",
-          Accept: "application/json, text/plain, */*",
-          "Accept-Language": "ru-RU,ru;q=0.8,en-US;q=0.5,en;q=0.3",
-        },
-        method: "GET",
-        mode: "cors",
-      }
-    ).then((res) => res.json());
+    try {
+      // Fetching csrf-token
+      const { csrftoken } = await fetch(
+        "https://manager.avocode.com/api/get-csrftoken/",
+        {
+          credentials: "include",
+          method: "GET",
+          mode: "cors",
+        }
+      ).then((res) => res.json());
 
-    console.log(`Check whether ${credentials.email} exists`);
-    const emailExistsResponse = await fetch(
-      "https://manager.avocode.com/api/account/email-exists/",
-      {
+      // Check whether email exists
+      await fetch("https://manager.avocode.com/api/account/email-exists/", {
         credentials: "include",
         headers: {
-          "User-Agent":
-            "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:75.0) Gecko/20100101 Firefox/75.0",
-          Accept: "application/json, text/plain, */*",
-          "Accept-Language": "ru-RU,ru;q=0.8,en-US;q=0.5,en;q=0.3",
-          "Content-Type": "application/json;charset=utf-8",
-          "X-CSRFToken": csrftoken.csrftoken,
+          "X-CSRFToken": csrftoken,
         },
         body: `{"email": "${credentials.email}" }`,
         method: "POST",
         mode: "cors",
-      }
-    ).then((res) => res.text());
+      });
 
-    console.log(`Add email ${credentials.email} as subscriber (dunno why)`);
-    const addSubscriberResponse = await fetch(
-      "https://pigeon.avocode.com/api/add-subscriber",
-      {
-        credentials: "omit",
-        headers: {
-          "User-Agent":
-            "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:75.0) Gecko/20100101 Firefox/75.0",
-          Accept: "application/json, text/plain, */*",
-          "Accept-Language": "ru-RU,ru;q=0.8,en-US;q=0.5,en;q=0.3",
-          "Content-Type": "application/json;charset=utf-8",
-        },
-        body: `{"email": "${credentials.email}", "source":"signup"}`,
-        method: "POST",
-        mode: "cors",
-      }
-    ).then((res) => res.json());
-
-    console.log(
-      `Register email: ${credentials.email} and password ${credentials.pass}`
-    );
-    const registerResponse = await fetch(
-      "https://manager.avocode.com/api/account/create/",
-      {
+      // Register account
+      await fetch("https://manager.avocode.com/api/account/create/", {
         credentials: "include",
         headers: {
           "User-Agent":
@@ -74,26 +39,28 @@ const register = async (page, credentials) => {
           Accept: "application/json, text/plain, */*",
           "Accept-Language": "ru-RU,ru;q=0.8,en-US;q=0.5,en;q=0.3",
           "Content-Type": "application/json;charset=utf-8",
-          "X-CSRFToken": csrftoken.csrftoken,
+          "X-CSRFToken": csrftoken,
         },
         body: `{"email": "${credentials.email}",
-        "password": "${credentials.pass}",
-        "signup_source":"index~hero",
-        "repeat_signup":false,
-        "plan":"teamwork-monthly",
-        "invited":false,
-        "invited_type":"n/a",
-        "onboarding_variant":
-        "remove-sidebar-add-invite-empty-state",
-        "onboarding_experiment":"044-remove-sidebar-add-invite-empty-state",
-        "onboarding_type":"regular",
-        "first_visited_page":"index",
-        "referrer_url":"$direct",
-        "referrer_domain":"$direct"}`,
+          "password": "${credentials.pass}",
+          "signup_source":"index~hero",
+          "repeat_signup":false,
+          "plan":"teamwork-monthly",
+          "invited":false,
+          "invited_type":"n/a",
+          "onboarding_variant":
+          "remove-sidebar-add-invite-empty-state",
+          "onboarding_experiment":"044-remove-sidebar-add-invite-empty-state",
+          "onboarding_type":"regular",
+          "first_visited_page":"index",
+          "referrer_url":"$direct",
+          "referrer_domain":"$direct"}`,
         method: "POST",
         mode: "cors",
-      }
-    ).then((res) => res.json());
+      });
+    } catch (e) {
+      return e;
+    }
   }, JSON.stringify(credentials));
 
   console.log(`I'm done with registration!`);
@@ -120,8 +87,7 @@ export const registerAvocodeTrialAccount = async () => {
       .match(/^<.+$/gm)
       .filter((string) => !string.includes("app.avocode.com"))
       .map((string) => {
-
-        return string.trim().substr(1, string.length - 2)
+        return string.trim().substr(1, string.length - 2);
       });
 
     return `*email:* ${credentials.email}\n*pass:* ${credentials.pass}\n*link: *${confirmationLink}`;
